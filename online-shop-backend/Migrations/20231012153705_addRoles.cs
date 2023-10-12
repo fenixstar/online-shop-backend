3c8a9f76a552aad206a9f147cc2955a1b861081f
Anton Gavrikov <gavrikovantonsergeevich@gmail.com>
2023-10-12 18:37:38 +0300

    Добавил роли

---
+++ online-shop-backend/Migrations/20231012153705_addRoles.cs
@@ -0,0 +1,218 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace online_shop_backend.Migrations
+{
+    /// <inheritdoc />
+    public partial class addRoles : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_AspNetRoleClaims_AspNetRoles_RoleId",
+                table: "AspNetRoleClaims");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_AspNetUserRoles_AspNetRoles_RoleId",
+                table: "AspNetUserRoles");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_AspNetRoles",
+                table: "AspNetRoles");
+
+            migrationBuilder.RenameTable(
+                name: "AspNetRoles",
+                newName: "roles");
+
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "ExpiryDate",
+                table: "refresh_tokens",
+                type: "timestamp with time zone",
+                nullable: false,
+                oldClrType: typeof(DateTime),
+                oldType: "timestamp without time zone");
+
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "DateAndTime",
+                table: "orders",
+                type: "timestamp with time zone",
+                nullable: false,
+                oldClrType: typeof(DateTime),
+                oldType: "timestamp without time zone");
+
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "DateIssued",
+                table: "invoices",
+                type: "timestamp with time zone",
+                nullable: false,
+                oldClrType: typeof(DateTime),
+                oldType: "timestamp without time zone");
+
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "StartTime",
+                table: "discounts",
+                type: "timestamp with time zone",
+                nullable: false,
+                oldClrType: typeof(DateTime),
+                oldType: "timestamp without time zone");
+
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "EndTime",
+                table: "discounts",
+                type: "timestamp with time zone",
+                nullable: false,
+                oldClrType: typeof(DateTime),
+                oldType: "timestamp without time zone");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Discriminator",
+                table: "AspNetUsers",
+                type: "character varying(21)",
+                maxLength: 21,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "text");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_roles",
+                table: "roles",
+                column: "Id");
+
+            migrationBuilder.InsertData(
+                table: "roles",
+                columns: new[] { "Id", "ConcurrencyStamp", "Name", "NormalizedName" },
+                values: new object[,]
+                {
+                    { "04be2a18-889f-491a-bea9-4f7c159e11bc", null, "User", "USER" },
+                    { "5d5f1611-12c7-4075-a568-f5feb8ccbaa7", null, "Admin", "ADMIN" },
+                    { "c4d31c79-0cf9-4f96-baff-1572c61f01d7", null, "Moderator", "MODERATOR" }
+                });
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_AspNetRoleClaims_roles_RoleId",
+                table: "AspNetRoleClaims",
+                column: "RoleId",
+                principalTable: "roles",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_AspNetUserRoles_roles_RoleId",
+                table: "AspNetUserRoles",
+                column: "RoleId",
+                principalTable: "roles",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_AspNetRoleClaims_roles_RoleId",
+                table: "AspNetRoleClaims");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_AspNetUserRoles_roles_RoleId",
+                table: "AspNetUserRoles");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_roles",
+                table: "roles");
+
+            migrationBuilder.DeleteData(
+                table: "roles",
+                keyColumn: "Id",
+                keyValue: "04be2a18-889f-491a-bea9-4f7c159e11bc");
+
+            migrationBuilder.DeleteData(
+                table: "roles",
+                keyColumn: "Id",
+                keyValue: "5d5f1611-12c7-4075-a568-f5feb8ccbaa7");
+
+            migrationBuilder.DeleteData(
+                table: "roles",
+                keyColumn: "Id",
+                keyValue: "c4d31c79-0cf9-4f96-baff-1572c61f01d7");
+
+            migrationBuilder.RenameTable(
+                name: "roles",
+                newName: "AspNetRoles");
+
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "ExpiryDate",
+                table: "refresh_tokens",
+                type: "timestamp without time zone",
+                nullable: false,
+                oldClrType: typeof(DateTime),
+                oldType: "timestamp with time zone");
+
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "DateAndTime",
+                table: "orders",
+                type: "timestamp without time zone",
+                nullable: false,
+                oldClrType: typeof(DateTime),
+                oldType: "timestamp with time zone");
+
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "DateIssued",
+                table: "invoices",
+                type: "timestamp without time zone",
+                nullable: false,
+                oldClrType: typeof(DateTime),
+                oldType: "timestamp with time zone");
+
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "StartTime",
+                table: "discounts",
+                type: "timestamp without time zone",
+                nullable: false,
+                oldClrType: typeof(DateTime),
+                oldType: "timestamp with time zone");
+
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "EndTime",
+                table: "discounts",
+                type: "timestamp without time zone",
+                nullable: false,
+                oldClrType: typeof(DateTime),
+                oldType: "timestamp with time zone");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Discriminator",
+                table: "AspNetUsers",
+                type: "text",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "character varying(21)",
+                oldMaxLength: 21);
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_AspNetRoles",
+                table: "AspNetRoles",
+                column: "Id");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_AspNetRoleClaims_AspNetRoles_RoleId",
+                table: "AspNetRoleClaims",
+                column: "RoleId",
+                principalTable: "AspNetRoles",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_AspNetUserRoles_AspNetRoles_RoleId",
+                table: "AspNetUserRoles",
+                column: "RoleId",
+                principalTable: "AspNetRoles",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}
