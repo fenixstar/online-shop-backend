4cc0bcc5f173347b94e6b9932fe17c6a3ec4cbe6
Anton Gavrikov <gavrikovantonsergeevich@gmail.com>
2023-10-13 15:20:46 +0300

    добавил категории

---
+++ online-shop-backend/Migrations/20231013121948_AddCategories.cs
@@ -0,0 +1,306 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace online_shop_backend.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddCategories : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "roles",
+                keyColumn: "Id",
+                keyValue: "04be2a18-889f-491a-bea9-4f7c159e11bc");
+
+            migrationBuilder.DeleteData(
+                table: "roles",
+                keyColumn: "Id",
+                keyValue: "5d5f1611-12c7-4075-a568-f5feb8ccbaa7");
+
+            migrationBuilder.DeleteData(
+                table: "roles",
+                keyColumn: "Id",
+                keyValue: "c4d31c79-0cf9-4f96-baff-1572c61f01d7");
+
+            migrationBuilder.InsertData(
+                table: "categories",
+                columns: new[] { "ID", "Name" },
+                values: new object[,]
+                {
+                    { 1, "Собаки" },
+                    { 2, "Кошки" },
+                    { 3, "Грызуны" },
+                    { 4, "Рыбы" },
+                    { 5, "Птицы" },
+                    { 6, "Риптилии" },
+                    { 7, "Другие питомцы" },
+                    { 8, "Уход и аксессуары" }
+                });
+
+            migrationBuilder.InsertData(
+                table: "roles",
+                columns: new[] { "Id", "ConcurrencyStamp", "Name", "NormalizedName" },
+                values: new object[,]
+                {
+                    { "16f7e772-cb28-4a63-86c8-da9839230c67", null, "Moderator", "MODERATOR" },
+                    { "7a0816aa-31a8-4dbf-8ed7-1d4ecd8dae8c", null, "User", "USER" },
+                    { "f7478dfe-7f6e-40e6-8f8b-a6ca3467246c", null, "Admin", "ADMIN" }
+                });
+
+            migrationBuilder.InsertData(
+                table: "subcategories",
+                columns: new[] { "ID", "CategoryID", "Name" },
+                values: new object[,]
+                {
+                    { 1, 1, "Корм для собак" },
+                    { 2, 1, "Игрушки для собак" },
+                    { 3, 1, "Ошейники и поводкик" },
+                    { 4, 1, "Туалеты" },
+                    { 5, 1, "Миски и поилки" },
+                    { 6, 2, "Корм для кошек" },
+                    { 7, 2, "Игрушки для кошек" },
+                    { 8, 2, "Ошейники и поводки" },
+                    { 9, 2, "Когтеточки" },
+                    { 10, 2, "Туалеты" },
+                    { 11, 2, "Миски и поилки" },
+                    { 12, 3, "Корм для грызунов" },
+                    { 13, 3, "Вольеры и клетки" },
+                    { 14, 3, "Игрушки для грызунов" },
+                    { 15, 3, "Миски и поилки" },
+                    { 16, 4, "Корм для рыбок" },
+                    { 17, 4, "Прочая переферия для рыбок" },
+                    { 18, 5, "Вольеры и клетки" },
+                    { 19, 5, "Корм для птиц" },
+                    { 20, 5, "Гнезда и гнездовые принадлежности" },
+                    { 21, 6, "Террариумы" },
+                    { 22, 6, "Корм для рептилий" },
+                    { 23, 6, "Освещение и обогрев для террариумов" },
+                    { 24, 7, "Мелкие животные" },
+                    { 25, 7, "Корм и аксессуары для экзотических животных" },
+                    { 26, 8, "Гигиенические средства" },
+                    { 27, 8, "Посуда и кормушки" },
+                    { 28, 8, "Лежанки и домики" },
+                    { 29, 8, "Одежда и аксессуары" }
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "roles",
+                keyColumn: "Id",
+                keyValue: "16f7e772-cb28-4a63-86c8-da9839230c67");
+
+            migrationBuilder.DeleteData(
+                table: "roles",
+                keyColumn: "Id",
+                keyValue: "7a0816aa-31a8-4dbf-8ed7-1d4ecd8dae8c");
+
+            migrationBuilder.DeleteData(
+                table: "roles",
+                keyColumn: "Id",
+                keyValue: "f7478dfe-7f6e-40e6-8f8b-a6ca3467246c");
+
+            migrationBuilder.DeleteData(
+                table: "subcategories",
+                keyColumn: "ID",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "subcategories",
+                keyColumn: "ID",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "subcategories",
+                keyColumn: "ID",
+                keyValue: 3);
+
+            migrationBuilder.DeleteData(
+                table: "subcategories",
+                keyColumn: "ID",
+                keyValue: 4);
+
+            migrationBuilder.DeleteData(
+                table: "subcategories",
+                keyColumn: "ID",
+                keyValue: 5);
+
+            migrationBuilder.DeleteData(
+                table: "subcategories",
+                keyColumn: "ID",
+                keyValue: 6);
+
+            migrationBuilder.DeleteData(
+                table: "subcategories",
+                keyColumn: "ID",
+                keyValue: 7);
+
+            migrationBuilder.DeleteData(
+                table: "subcategories",
+                keyColumn: "ID",
+                keyValue: 8);
+
+            migrationBuilder.DeleteData(
+                table: "subcategories",
+                keyColumn: "ID",
+                keyValue: 9);
+
+            migrationBuilder.DeleteData(
+                table: "subcategories",
+                keyColumn: "ID",
+                keyValue: 10);
+
+            migrationBuilder.DeleteData(
+                table: "subcategories",
+                keyColumn: "ID",
+                keyValue: 11);
+
+            migrationBuilder.DeleteData(
+                table: "subcategories",
+                keyColumn: "ID",
+                keyValue: 12);
+
+            migrationBuilder.DeleteData(
+                table: "subcategories",
+                keyColumn: "ID",
+                keyValue: 13);
+
+            migrationBuilder.DeleteData(
+                table: "subcategories",
+                keyColumn: "ID",
+                keyValue: 14);
+
+            migrationBuilder.DeleteData(
+                table: "subcategories",
+                keyColumn: "ID",
+                keyValue: 15);
+
+            migrationBuilder.DeleteData(
+                table: "subcategories",
+                keyColumn: "ID",
+                keyValue: 16);
+
+            migrationBuilder.DeleteData(
+                table: "subcategories",
+                keyColumn: "ID",
+                keyValue: 17);
+
+            migrationBuilder.DeleteData(
+                table: "subcategories",
+                keyColumn: "ID",
+                keyValue: 18);
+
+            migrationBuilder.DeleteData(
+                table: "subcategories",
+                keyColumn: "ID",
+                keyValue: 19);
+
+            migrationBuilder.DeleteData(
+                table: "subcategories",
+                keyColumn: "ID",
+                keyValue: 20);
+
+            migrationBuilder.DeleteData(
+                table: "subcategories",
+                keyColumn: "ID",
+                keyValue: 21);
+
+            migrationBuilder.DeleteData(
+                table: "subcategories",
+                keyColumn: "ID",
+                keyValue: 22);
+
+            migrationBuilder.DeleteData(
+                table: "subcategories",
+                keyColumn: "ID",
+                keyValue: 23);
+
+            migrationBuilder.DeleteData(
+                table: "subcategories",
+                keyColumn: "ID",
+                keyValue: 24);
+
+            migrationBuilder.DeleteData(
+                table: "subcategories",
+                keyColumn: "ID",
+                keyValue: 25);
+
+            migrationBuilder.DeleteData(
+                table: "subcategories",
+                keyColumn: "ID",
+                keyValue: 26);
+
+            migrationBuilder.DeleteData(
+                table: "subcategories",
+                keyColumn: "ID",
+                keyValue: 27);
+
+            migrationBuilder.DeleteData(
+                table: "subcategories",
+                keyColumn: "ID",
+                keyValue: 28);
+
+            migrationBuilder.DeleteData(
+                table: "subcategories",
+                keyColumn: "ID",
+                keyValue: 29);
+
+            migrationBuilder.DeleteData(
+                table: "categories",
+                keyColumn: "ID",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "categories",
+                keyColumn: "ID",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "categories",
+                keyColumn: "ID",
+                keyValue: 3);
+
+            migrationBuilder.DeleteData(
+                table: "categories",
+                keyColumn: "ID",
+                keyValue: 4);
+
+            migrationBuilder.DeleteData(
+                table: "categories",
+                keyColumn: "ID",
+                keyValue: 5);
+
+            migrationBuilder.DeleteData(
+                table: "categories",
+                keyColumn: "ID",
+                keyValue: 6);
+
+            migrationBuilder.DeleteData(
+                table: "categories",
+                keyColumn: "ID",
+                keyValue: 7);
+
+            migrationBuilder.DeleteData(
+                table: "categories",
+                keyColumn: "ID",
+                keyValue: 8);
+
+            migrationBuilder.InsertData(
+                table: "roles",
+                columns: new[] { "Id", "ConcurrencyStamp", "Name", "NormalizedName" },
+                values: new object[,]
+                {
+                    { "04be2a18-889f-491a-bea9-4f7c159e11bc", null, "User", "USER" },
+                    { "5d5f1611-12c7-4075-a568-f5feb8ccbaa7", null, "Admin", "ADMIN" },
+                    { "c4d31c79-0cf9-4f96-baff-1572c61f01d7", null, "Moderator", "MODERATOR" }
+                });
+        }
+    }
+}
--- online-shop-backend/Models/Identity/ApplicationDbContext.cs
+++ online-shop-backend/Models/Identity/ApplicationDbContext.cs
@@ -5,57 +5,98 @@ using Microsoft.EntityFrameworkCore;
 using online_shop_backend.Models.Entities;
 using online_shop_backend.Utils;
 
-namespace online_shop_backend.Models.Identity
+namespace online_shop_backend.Models.Identity;
+
+public class ApplicationDbContext : IdentityDbContext
 {
-    public class ApplicationDbContext : IdentityDbContext
+    public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
     {
-        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
-        {
-        }
+    }
 
-        public DbSet<UserDetail> UserDetails { get; set; }
-        public DbSet<Product> Products { get; set; }
-        public DbSet<Producer> Producers { get; set; }
-        public DbSet<ProducerDetail> ProducerDetails { get; set; }
-        public DbSet<Order> Orders { get; set; }
-        public DbSet<OrderDetail> OrderDetails { get; set; }
-        public DbSet<Category> Categories { get; set; }
-        public DbSet<Subcategory> Subcategories { get; set; }
-        public DbSet<PaymentType> PaymentTypes { get; set; }
-        public DbSet<PaymentMethod> PaymentMethods { get; set; }
-        public DbSet<Review> Reviews { get; set; }
-        public DbSet<ShippingMethod> ShippingMethods { get; set; }
-        public DbSet<Invoice> Invoices { get; set; }
-        public DbSet<InvoiceDetail> InvoiceDetails { get; set; }
-        public DbSet<Discount> Discounts { get; set; }
-        public DbSet<RefreshToken> RefreshTokens { get; set; }
-        public DbSet<IdentityRole> Roles { get; set; }
+    public DbSet<UserDetail> UserDetails { get; set; }
+    public DbSet<Product> Products { get; set; }
+    public DbSet<Producer> Producers { get; set; }
+    public DbSet<ProducerDetail> ProducerDetails { get; set; }
+    public DbSet<Order> Orders { get; set; }
+    public DbSet<OrderDetail> OrderDetails { get; set; }
+    public DbSet<Category> Categories { get; set; }
+    public DbSet<Subcategory> Subcategories { get; set; }
+    public DbSet<PaymentType> PaymentTypes { get; set; }
+    public DbSet<PaymentMethod> PaymentMethods { get; set; }
+    public DbSet<Review> Reviews { get; set; }
+    public DbSet<ShippingMethod> ShippingMethods { get; set; }
+    public DbSet<Invoice> Invoices { get; set; }
+    public DbSet<InvoiceDetail> InvoiceDetails { get; set; }
+    public DbSet<Discount> Discounts { get; set; }
+    public DbSet<RefreshToken> RefreshTokens { get; set; }
+    public DbSet<IdentityRole> Roles { get; set; }
 
 
-        protected override void OnModelCreating(ModelBuilder modelBuilder)
-        {
-            base.OnModelCreating(modelBuilder);
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder);
 
-            modelBuilder.Entity<UserDetail>().ToTable("user_details");
-            modelBuilder.Entity<Product>().ToTable("products");
-            modelBuilder.Entity<Producer>().ToTable("producers");
-            modelBuilder.Entity<ProducerDetail>().ToTable("producer_details");
-            modelBuilder.Entity<Order>().ToTable("orders");
-            modelBuilder.Entity<OrderDetail>().ToTable("order_details");
-            modelBuilder.Entity<Category>().ToTable("categories");
-            modelBuilder.Entity<Subcategory>().ToTable("subcategories");
-            modelBuilder.Entity<PaymentType>().ToTable("payment_types");
-            modelBuilder.Entity<PaymentMethod>().ToTable("payment_methods");
-            modelBuilder.Entity<Review>().ToTable("reviews");
-            modelBuilder.Entity<ShippingMethod>().ToTable("shipping_methods");
-            modelBuilder.Entity<Invoice>().ToTable("invoices");
-            modelBuilder.Entity<InvoiceDetail>().ToTable("invoice_details");
-            modelBuilder.Entity<Discount>().ToTable("discounts");
-            modelBuilder.Entity<RefreshToken>().ToTable("refresh_tokens");
-            modelBuilder.Entity<IdentityRole>().ToTable("roles")
-                .HasData(new { Id = Guid.NewGuid().ToString(), Name = Constants.USER, NormalizedName = Constants.USER.ToUpper() },
-                    new { Id = Guid.NewGuid().ToString(), Name = Constants.ADMIN, NormalizedName = Constants.ADMIN.ToUpper() },
-                    new { Id = Guid.NewGuid().ToString(), Name = Constants.MODERATOR, NormalizedName = Constants.MODERATOR.ToUpper() });
-        }
+        modelBuilder.Entity<UserDetail>().ToTable("user_details");
+        modelBuilder.Entity<Product>().ToTable("products");
+        modelBuilder.Entity<Producer>().ToTable("producers");
+        modelBuilder.Entity<ProducerDetail>().ToTable("producer_details");
+        modelBuilder.Entity<Order>().ToTable("orders");
+        modelBuilder.Entity<OrderDetail>().ToTable("order_details");
+        modelBuilder.Entity<Category>()
+            .ToTable("categories")
+            .HasData(
+                new Category { ID = 1, Name = "Собаки" },
+                new Category { ID = 2, Name = "Кошки" },
+                new Category { ID = 3, Name = "Грызуны" },
+                new Category { ID = 4, Name = "Рыбы" },
+                new Category { ID = 5, Name = "Птицы" },
+                new Category { ID = 6, Name = "Риптилии" },
+                new Category { ID = 7, Name = "Другие питомцы" },
+                new Category { ID = 8, Name = "Уход и аксессуары" }
+            );
+        modelBuilder.Entity<Subcategory>()
+            .ToTable("subcategories")
+            .HasData(
+                new Subcategory { ID = 1, CategoryID = 1, Name = "Корм для собак" },
+                new Subcategory { ID = 2, CategoryID = 1, Name = "Игрушки для собак" },
+                new Subcategory { ID = 3, CategoryID = 1, Name = "Ошейники и поводкик" },
+                new Subcategory { ID = 4, CategoryID = 1, Name = "Туалеты" },
+                new Subcategory { ID = 5, CategoryID = 1, Name = "Миски и поилки" },
+                new Subcategory { ID = 6, CategoryID = 2, Name = "Корм для кошек" },
+                new Subcategory { ID = 7, CategoryID = 2, Name = "Игрушки для кошек" },
+                new Subcategory { ID = 8, CategoryID = 2, Name = "Ошейники и поводки" },
+                new Subcategory { ID = 9, CategoryID = 2, Name = "Когтеточки" },
+                new Subcategory { ID = 10, CategoryID = 2, Name = "Туалеты" },
+                new Subcategory { ID = 11, CategoryID = 2, Name = "Миски и поилки" },
+                new Subcategory { ID = 12, CategoryID = 3, Name = "Корм для грызунов" },
+                new Subcategory { ID = 13, CategoryID = 3, Name = "Вольеры и клетки" },
+                new Subcategory { ID = 14, CategoryID = 3, Name = "Игрушки для грызунов" },
+                new Subcategory { ID = 15, CategoryID = 3, Name = "Миски и поилки" },
+                new Subcategory { ID = 16, CategoryID = 4, Name = "Корм для рыбок" },
+                new Subcategory { ID = 17, CategoryID = 4, Name = "Прочая переферия для рыбок" },
+                new Subcategory { ID = 18, CategoryID = 5, Name = "Вольеры и клетки" },
+                new Subcategory { ID = 19, CategoryID = 5, Name = "Корм для птиц" },
+                new Subcategory { ID = 20, CategoryID = 5, Name = "Гнезда и гнездовые принадлежности" },
+                new Subcategory { ID = 21, CategoryID = 6, Name = "Террариумы" },
+                new Subcategory { ID = 22, CategoryID = 6, Name = "Корм для рептилий" },
+                new Subcategory { ID = 23, CategoryID = 6, Name = "Освещение и обогрев для террариумов" },
+                new Subcategory { ID = 24, CategoryID = 7, Name = "Мелкие животные" },
+                new Subcategory { ID = 25, CategoryID = 7, Name = "Корм и аксессуары для экзотических животных" },
+                new Subcategory { ID = 26, CategoryID = 8, Name = "Гигиенические средства" },
+                new Subcategory { ID = 27, CategoryID = 8, Name = "Посуда и кормушки" },
+                new Subcategory { ID = 28, CategoryID = 8, Name = "Лежанки и домики" },
+                new Subcategory { ID = 29, CategoryID = 8, Name = "Одежда и аксессуары" });
+        modelBuilder.Entity<PaymentType>().ToTable("payment_types");
+        modelBuilder.Entity<PaymentMethod>().ToTable("payment_methods");
+        modelBuilder.Entity<Review>().ToTable("reviews");
+        modelBuilder.Entity<ShippingMethod>().ToTable("shipping_methods");
+        modelBuilder.Entity<Invoice>().ToTable("invoices");
+        modelBuilder.Entity<InvoiceDetail>().ToTable("invoice_details");
+        modelBuilder.Entity<Discount>().ToTable("discounts");
+        modelBuilder.Entity<RefreshToken>().ToTable("refresh_tokens");
+        modelBuilder.Entity<IdentityRole>().ToTable("roles")
+            .HasData(new { Id = Guid.NewGuid().ToString(), Name = Constants.USER, NormalizedName = Constants.USER.ToUpper() },
+                new { Id = Guid.NewGuid().ToString(), Name = Constants.ADMIN, NormalizedName = Constants.ADMIN.ToUpper() },
+                new { Id = Guid.NewGuid().ToString(), Name = Constants.MODERATOR, NormalizedName = Constants.MODERATOR.ToUpper() });
     }
 }
